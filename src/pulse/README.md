afb1773c223512735222d2e9664d93689ee39b1b
guy sulema <suli@virtue.im>
2019-07-24 12:43:34 +0300

    add readme

--- src/pulse/README.md
+++ src/pulse/README.md
@@ -1,4 +1,20 @@
 # Docker-Pulse Secure-Integration
-The docker contains a running Logstash agent. The docker is collecting data from [Pulse-Secure](https://www.pulsesecure.net/) (using syslog) and sends the log data to Bridgecrew cloud in an encrypted secured manner.
+The docker contains a running Logstash agent. The docker is collecting data from [Pulse Secure Virtual Traffic Manager](https://www.pulsesecure.net/) (using syslog) and sends the log data to Bridgecrew cloud in an encrypted secured manner.
 
 ## Pulse Secure
+ Two parts for installation:
+ 1) Configuring the Traffic Manager to Export Data to Logstash
+ 2) Installing the Pulse Secure integration docker 
+ 
+ 
+ ### 1. Pulse Secure Traffic Manager syslog configuration
+ Enter to [guide](https://www-prev.pulsesecure.net/download/techpubs/current/1416/Pulse-vADC-Solutions/Pulse-Virtual-Traffic-Manager/18.2/DeploymentGuide_vTM-ElasticStack.pdf) page 7-8 and follow after the instructions.
+    
+#### Transaction Metadata  
+**transaction_export!endpoint:** IP_SERVER_DOCKER:PORT (For example -localhost:9910)  
+**transaction_export!tls:**  No  
+**transaction_export!enabled:** Yes    
+
+#### Log Files
+**log_export!endpoint:** IP_SERVER_DOCKER:PORT (For example -localhost:9910)
+**log_export!enabled:** Yes
--- test/fortigate/index.js
+++ test/fortigate/index.js
@@ -20,11 +20,11 @@ Promise.resolve().then(() => {
   // return Promise.all(promiseArray);
 }).then(() => {
   process.exit();
-})
+});
   
-  function sendMessage(clientInstance, port,level,yaer) {
+  function sendMessage(clientInstance, port,level,year) {
     return new Promise((resolve,reject) => {
-      clientInstance.send(`<22>date=${yaer}-1-15 time=11:44:16 logid="0000000013" type="traffic" subtype="forward" level="${level}" vd="vdom1" eventtime=1510775056 srcip=10.1.100.155 srcname="pc1" srcport=40772 srcintf="port12" srcintfrole="undefined" dstip=35.197.51.42 dstname="fortiguard.com" dstport=443 dstintf="port11" dstintfrole="undefined" poluuid="707a0d88-c972-51e7-bbc7-4d421660557b" sessionid=8058 proto=6 action="close" policyid=1 policytype="policy" policymode="learn" service="HTTPS" dstcountry="United States" srccountry="Reserved" trandisp="snat" transip=172.16.200.2 transport=40772 appid=40568 app="HTTPS.BROWSER" appcat="Web.Client" apprisk="medium" duration=2 sentbyte=1850 rcvdbyte=39898 sentpkt=25 rcvdpkt=37 utmaction="allow" countapp=1 devtype="Linux PC" osname="Linux" mastersrcmac="a2:e9:00:ec:40:01" srcmac="a2:e9:00:ec:40:01" srcserver=0 utmref=0-220586`,
+      clientInstance.send(`<22>date=${year}-1-15 time=11:44:16 logid="0000000013" type="traffic" subtype="forward" level="${level}" vd="vdom1" eventtime=1510775056 srcip=10.1.100.155 srcname="pc1" srcport=40772 srcintf="port12" srcintfrole="undefined" dstip=35.197.51.42 dstname="fortiguard.com" dstport=443 dstintf="port11" dstintfrole="undefined" poluuid="707a0d88-c972-51e7-bbc7-4d421660557b" sessionid=8058 proto=6 action="close" policyid=1 policytype="policy" policymode="learn" service="HTTPS" dstcountry="United States" srccountry="Reserved" trandisp="snat" transip=172.16.200.2 transport=40772 appid=40568 app="HTTPS.BROWSER" appcat="Web.Client" apprisk="medium" duration=2 sentbyte=1850 rcvdbyte=39898 sentpkt=25 rcvdpkt=37 utmaction="allow" countapp=1 devtype="Linux PC" osname="Linux" mastersrcmac="a2:e9:00:ec:40:01" srcmac="a2:e9:00:ec:40:01" srcserver=0 utmref=0-220586`,
       port,'127.0.0.1',function(error){
           if(error){
             client.close();
